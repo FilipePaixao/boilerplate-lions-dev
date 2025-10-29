ab9210cbbc98f938588c1578ccacd04a6c2d0c91
Filipe Paixão <fpedrapaixao@gmail.com>
2025-10-29 11:19:18 -0300

    refactor: convert user.repository.js to ES6 module syntax

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,20 +1,22 @@
-const User = require('../models/user.model');
+import User from '../models/user.model.js';
 
-module.exports = {
-  create(data) { 
-    return User.create(data); 
+export default {
+  create(data) {
+    return User.create(data);
   },
-  findAll() { return User.find(); },
-
-  findById(id) { return User.findById(id); },
-  updateById(id, data) { return User.findByIdAndUpdate(id, data, { new: true }); },
-  deleteById(id) { return User.findByIdAndDelete(id); },
-  
-  findUserByEmail(data){
-    try {
-      return User.findOne({ email: data.email });
-    } catch (error) {
-      throw new Error('Error finding user by email: ' + error.message);
-    }
-  }
+  findAll() {
+    return User.find();
+  },
+  findById(id) {
+    return User.findById(id);
+  },
+  updateById(id, data) {
+    return User.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+  },
+  deleteById(id) {
+    return User.findByIdAndDelete(id);
+  },
+  findByEmail(email) {
+    return User.findOne({ email });
+  },
 };
