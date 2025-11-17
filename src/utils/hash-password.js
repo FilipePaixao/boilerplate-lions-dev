483b43f205eb7f364e0f0f06b61d2c5d4d11804b
Filipe Paixão <fpedrapaixao@gmail.com>
2025-11-17 11:05:47 -0300

    refactor: update compareHashedPassword to named export in hash-password.js

--- src/utils/hash-password.js
+++ src/utils/hash-password.js
@@ -3,6 +3,6 @@ export default function hashPassword(password) {
   return bcrypt.hashSync(password, 10);
 }
 
-export default function compareHashedPassword(password, hashedPassword) {
+export function compareHashedPassword(password, hashedPassword) {
   return bcrypt.compareSync(password, hashedPassword);
 }
